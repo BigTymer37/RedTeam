2873792a8f63617e851ccaff28d6c5ef42f800d0
Eric Van Zutphen <ericv_37@hotmail.com>
2022-02-15 11:09:43 -0500

    find-transform.py
    This scripts assist in finding the transform for a IPSEC IKE service for different VPN appliances.

---
+++ find-transform.py
@@ -0,0 +1,32 @@
+import subprocess
+
+transforms = []
+
+ip_address = input("What IP address do you want to scan? ")
+
+def create_transforms():
+        encryptions = ["1","2","3","4","5","6","7/128","7/192","7/256","8"]
+        hashes = ["1","2","3","4","5","6"]
+        authentications = ["1","2","3","4","5","6","7","8","64221","64222","64223","64224","65001","65002","65003","65004","65005","65006","65006","65007","65008","65009","65010"]
+        groups = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18"]
+        for encryption in encryptions:
+            for hash in hashes:
+                for authentication in authentications:
+                    for group in groups:
+                        print("--trans="+encryption+','+hash+','+authentication+','+group)
+                        transform = ("--trans="+encryption+','+hash+','+authentication+','+group)
+                        transforms.append(transform)
+create_transforms()
+
+def find_transforms():
+        try:
+            for transform in transforms:
+                try:
+                    command = str("""ike-scan -M -A %s %s -P hash.txt""") % (transform.strip(),ip_address)
+                    print(command)
+                    findtransform_results = subprocess.Popen((command),shell=True,stderr=subprocess.PIPE,stdout=subprocess.PIPE)
+                    (findtransform_results_stdout, findtransform_results_stderr) = findtransform_results.communicate()
+                    print(findgroup_results_stdout)
+                    print(findgroup_results_stderr)
+
+find_transforms()
