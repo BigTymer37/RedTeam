06f33b71316456681772f4eafafe73d62912a0a7
Eric Van Zutphen <ericv_37@hotmail.com>
2022-02-15 12:11:14 -0500

    Update find-transform.py

--- find-transform.py
+++ find-transform.py
@@ -14,6 +14,7 @@ def create_transforms():
             for hash in hashes:
                 for authentication in authentications:
                     for group in groups:
+                        print("--trans="+encryption+','+hash+','+authentication+','+group)
                         transform = ("--trans="+encryption+','+hash+','+authentication+','+group)
                         transforms.append(transform)
 create_transforms()
@@ -22,15 +23,16 @@ def find_transforms():
         try:
             for transform in transforms:
                 try:
-                    command = str("""ike-scan -M -A %s %s -P hash.txt""") % (transform.strip(),ip_address)
+                    command = str("""ike-scan -M -A %s %s -P hash.txt""") % (transform[:-1],ip_address)
                     print(command)
                     findtransform_results = subprocess.Popen((command),shell=True,stderr=subprocess.PIPE,stdout=subprocess.PIPE)
                     (findtransform_results_stdout, findtransform_results_stderr) = findtransform_results.communicate()
-                    print(findgroup_results_stdout)
-                    print(findgroup_results_stderr)
-                    ike_scan_outputs = findtransform_results_stdout.split()
-                    if ike_scan_outputs[33] == '1':
-                        print "Valid Transform Found: %s" % (transform.strip())
+    				print(findtransform_results_stdout)
+   	 				print(findtransform_results_stderr)
+    				ike_scan_outputs = findtransform_results_stdout.split()
+    				print(ike_scan_outputs[33])
+    				if ike_scan_outputs[33] == '1':
+        				print("Valid Transform Found: %s" % (transform.strip()))
                 except:
                 	pass
         except:
